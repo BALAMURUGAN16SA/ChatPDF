96861cea6614afbbfcab02ac92f870a765950e68
BALAMURUGAN16SA <136428164+BALAMURUGAN16SA@users.noreply.github.com>
2025-07-23 21:29:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ from langchain.prompts import PromptTemplate
 # from dotenv import load_dotenv
 # load_dotenv()
 # genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
-
+st.write("Secrets loaded:", st.secrets)
 genai.configure(api_key=st.secrets["api"]["gemini_key"])
 pytesseract.pytesseract.tesseract_cmd = shutil.which("tesseract")
 
