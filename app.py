e5224c2cf9bf0d5d309ac9f3b940f4f65bd1ac5b
BALAMURUGAN16SA <136428164+BALAMURUGAN16SA@users.noreply.github.com>
2025-07-23 21:05:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,8 +13,12 @@ from langchain_community.vectorstores.faiss import FAISS
 from langchain.chains.question_answering import load_qa_chain
 from langchain.prompts import PromptTemplate
 
-genai.configure(api_key=st.secrets["api"]["gemini_key"])
-print(st.secrets["api"]["gemini_key"])
+import os
+from dotenv import load_dotenv
+load_dotenv()
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+# genai.configure(api_key=st.secrets["api"]["gemini_key"])
 pytesseract.pytesseract.tesseract_cmd = shutil.which("tesseract")
 
 def extract_texts_from_pdfs(pdfs):
